620092cfe12aff485571bb2e78d54b60aae27b29
Viktoriia <vis22chu@gmail.com>
2020-12-21 02:58:23 +0400

    feat: add table.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   mode:"development",
-    entry: [`./src/js/list.js`],
+    entry: [`./src/js/list.js`, `./src/js/table.js`],
     output: {
       path: path.resolve('./src/build/'),
       filename: 'bundle.js'

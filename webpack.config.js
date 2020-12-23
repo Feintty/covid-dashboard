f4773a59dacf5f992b4af6e39216868777507a21
Viktoriia <vis22chu@gmail.com>
2020-12-23 13:11:47 +0400

    feat: add graphics

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   mode:"development",
-    entry: [`./src/js/list.js`, `./src/js/table.js`],
+    entry: [`./src/js/list.js`, `./src/js/table.js`, `./src/js/graphic.js`],
     output: {
       path: path.resolve('./src/build/'),
       filename: 'bundle.js'
